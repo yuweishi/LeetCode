4e68954cc785a8b43f8ff31294bca7ec1c661ca2
Yuwei Shi <christen112358@gmail.com>
2015-09-01 15:15:53 -0700

    first commit

---
+++ Algorithms/Two Sum/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] in  dict:
+                return dict[nums[i]], i + 1
+            dict[target - nums[i]] = i + 1
