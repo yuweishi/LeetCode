b837bd89bffb76a3d96cd3c65ad012d57fcdaa0c
Yuwei Shi <christen112358@gmail.com>
2015-09-14 00:21:36 -0700

    Update solution.py

--- Algorithms/Factor Combination/solution.py
+++ Algorithms/Factor Combination/solution.py
@@ -8,12 +8,8 @@ class Solution(object):
         self.factors(2, n, [], res)
         return res
     def factors(self, start, n, pre, res):
-        if n == 1:
-            if len(pre) > 1:
-                res += [pre]
-            return
         while start ** 2 <= n:
             if n % start == 0:
+                res += [pre + [start, n / start]]
                 self.factors(start, n / start, pre + [start], res)
             start += 1
-        self.factors(n, 1, pre + [n], res)
