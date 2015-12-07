bd468c492baae3de9960f40e70ff10523bd9a63e
Yuwei Shi <christen112358@gmail.com>
2015-12-06 23:56:51 -0800

    first commit

---
+++ Algorithms/Burst Balloons/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #discard all 0s in nums
+        nums_new = [1] + [num for num in nums if num > 0] + [1]
+        n = len(nums_new)
+        dp = [[0] * n for _ in xrange(n)]
+        #dp[i][j] represent the max value we can obtain by burst the balloons within i and j
+        for k in xrange(2, n):
+            for left in xrange(0, n - k):
+                right = left + k
+                for i in xrange(left + 1, right):
+                    dp[left][right] = max(dp[left][right],
+                           nums_new[left] * nums_new[i] * nums_new[right] + dp[left][i] + dp[i][right])
+        return dp[0][n - 1]
